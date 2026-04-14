158a5ac670703668ad3274c42bdaa778f82b1891
Atif-Bashir-1998 <adobe8832289@gmail.com>
2026-04-14 09:53:49 +0300

    unit module is properly documented

--- async-linguist-api/src/entities/unit.entity.ts
+++ async-linguist-api/src/entities/unit.entity.ts
@@ -1,24 +1,23 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToOne,
-  OneToMany,
-} from 'typeorm';
+// src/entities/unit.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 import { Course } from './course.entity';
 import { Sentence } from './sentence.entity';
 
 @Entity('units')
 export class Unit {
+  @ApiProperty({ example: 1, description: 'The unique ID of the unit' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'Basic Greetings', description: 'The title of the learning unit' })
   @Column()
   title: string;
 
   @ManyToOne(() => Course, (course) => course.units, { onDelete: 'CASCADE' })
   course: Course;
 
+  @ApiProperty({ type: () => [Sentence], description: 'List of sentences belonging to this unit' })
   @OneToMany(() => Sentence, (sentence) => sentence.unit)
   sentences: Sentence[];
 }
--- async-linguist-api/src/modules/units/dto/update-unit.dto.ts
+++ async-linguist-api/src/modules/units/dto/update-unit.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+// src/units/dto/update-unit.dto.ts
+import { ApiProperty, PartialType } from '@nestjs/swagger'; // Switch to Swagger PartialType
 import { CreateUnitDto } from './create-unit.dto';
 
 export class UpdateUnitDto extends PartialType(CreateUnitDto) {}
