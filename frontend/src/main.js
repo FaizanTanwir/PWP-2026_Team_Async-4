c51808a11bafce61afb6aefcce22f659815a816e
Atif-Bashir-1998 <adobe8832289@gmail.com>
2026-05-15 11:27:13 +0300

    added vue-toastification to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,20 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const toastificationOptions = {
+    timeout: 2000,
+    position: POSITION.TOP_RIGHT,    
+};
 
 const pinia = createPinia()
 
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
+app.use(Toast, toastificationOptions);
 
 app.mount('#app')
 
